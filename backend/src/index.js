98496e88c14e6282c1ee49c92a866ff0c0a81ef8
ansh soni <anshsoni200002@gmail.com>
2025-05-25 12:31:15 +0530

    somoe changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,18 +2,28 @@ require("dotenv").config()
 const express = require("express")
 const app = express();
 const cookieParser = require("cookie-parser")
+const cors = require("cors")
 const {connectDB} = require("./lib/db")
 const authRoutes = require("./routes/auth.route")
 const messageRoutes = require("./routes/message.route")
 
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}; 
+
+app.use(cors(corsOptions))
+app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
-// console.log(process.env.JWT_SECRET)
 
 
 app.use('/api/auth' , authRoutes)
 app.use('/api/message' , messageRoutes)
+
 
 
 app.listen(process.env.PORT , () => {
