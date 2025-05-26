3177b30478d0fdcace74dbd8db8931f07d23c51c
ansh soni <anshsoni200002@gmail.com>
2025-05-27 01:26:58 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,8 @@ const {connectDB} = require("./lib/db")
 const authRoutes = require("./routes/auth.route")
 const messageRoutes = require("./routes/message.route")
 const {app , server} = require("./lib/socket")
-
+const path = require("path")
+const __dirname = path.resolve()
 
 const corsOptions = {
   origin: 'http://localhost:5173',
@@ -25,6 +26,13 @@ app.use(express.urlencoded({extended:true}))
 app.use('/api/auth' , authRoutes)
 app.use('/api/messages' , messageRoutes)
 
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname , "../frontend/dist")))
+
+  app.get("*" , (req , res) => {
+    res.sendFile(path.join(__dirname , "../frontend" , "dist" , "index.html"));
+  })
+}
 
 
 server.listen(process.env.PORT , () => {
